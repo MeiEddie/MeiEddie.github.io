f5872e5be42b953d703383db7e0ade1f88a5022d
gitMei05 <3648939315@qq.com>
2026-07-17 14:38:45 +0800

    7.17四版

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   base: "/",
   title: "Eddieの小窝",
-  description: "A blog",
+  description: "一个小小的博客",
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
@@ -14,6 +14,7 @@ export default defineConfig({
         text: '我的文章',
         items: [
           { text: '简介', link: '/简介' },
+          { text: 'HTML学习', link: '/HTML学习' },
           { text: 'Markdown学习', link: '/Markdown学习' }
         ]
       },
